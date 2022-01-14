1158c5981b477c24b78b14f87778f009bcd36170
Raju Waghmare <92024085+Rajuwaghmare123@users.noreply.github.com>
2022-01-14 10:46:26 +0530

    Add files via upload

---
+++ Intersection.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+const getIntersectionNode = (headA, headB) => {
+    
+    if(!headA || !headB) return null;
+    
+    let runnerA = headA;
+    let runnerB = headB;
+   
+    while(runnerA !== runnerB){
+        if(!runnerA){
+            runnerA = headB;
+        } else{
+            runnerA = runnerA.next
+        }
+        
+        if(!runnerB){
+            runnerB = headA;
+        } else {
+            runnerB = runnerB.next;
+        }   
+    }
+    return runnerA;
+};
---
+++ RemoveDuplicate.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+var deleteDuplicates = function(head) {
+    if(head === null || head.next === null) 
+        return head;
+    
+    let prev = head;
+    let curr = head.next;
+    
+    while(curr !== null) {
+        if(prev.val === curr.val) {
+            let temp = curr;
+            prev.next = curr.next;
+            curr = curr.next;
+            temp = null;
+        } else {
+            prev = prev.next;
+            curr = curr.next;
+        }
+    }
+    return head;
+};
